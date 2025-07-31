e803692be8c8a926fdf22fef14b50d2af0ea51b4
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-07-31 16:40:59 +0530

    gemini_joke_bot

--- groq-site-summarizer/website-summarizer.py
+++ groq-site-summarizer/website-summarizer.py
@@ -16,7 +16,7 @@ api_key = os.getenv('GROQ_API_KEY')
 if not api_key:
     print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
 elif not api_key.startswith("gsk_"):
-    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+    print("An API key was found, but it doesn't start gsk_; please check you're using the right key - see troubleshooting notebook")
 elif api_key.strip() != api_key:
     print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
 else:
---
+++ joke-generator/gemini_joke_bot.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai
+
+
+load_dotenv(override=True)
+google_api_key = os.getenv('GOOGLE_API_KEY')
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:8]}")
+else:
+    print("Google API Key not set")
+
+google.generativeai.configure()
+system_message = "You are an assistant that is great at telling jokes"
+user_prompt = "Tell a light-hearted joke for an school kids"
+
+gemini = google.generativeai.GenerativeModel(
+    model_name='gemini-2.0-flash',
+    system_instruction=system_message
+)
+response = gemini.generate_content(user_prompt)
+print(response.text)
